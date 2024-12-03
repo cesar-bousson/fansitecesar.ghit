5004305ddc0fb3cd4c6d6d849bcc2ccbace7a644
cesar-bousson <cesar.bousson@laplateforme.io>
2024-12-03 13:45:47 +0100

    premier commit page perso fansite cesar

---
+++ Script2/paulk.javascript.js
@@ -0,0 +1,9 @@
+
+<script>
+        const menuToggle = document.getElementById("menuToggle");
+        const navigation = document.querySelector(".navigation");
+    
+        menuToggle.addEventListener("click", () = {'>'} {
+         navigation.classList.toggle("active") 
+            });
+    </script>
